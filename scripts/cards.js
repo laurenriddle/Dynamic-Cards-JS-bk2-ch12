22c1edf6fe7848183b2857ac2fead5f2aadf5548
Lauren Riddle <lari7132@gmail.com>
2019-10-18 14:40:51 -0500

    added a card in the html and got it to delete

---
+++ scripts/cards.js
@@ -0,0 +1,35 @@
+// console.log("TEST!")
+const container = document.querySelector("#container")
+let cardNumber = 1
+
+let cardObject = {
+    cardCreator: function () {
+        const text = document.querySelector("#text").value
+        let createCard = container.innerHTML +=
+            `<article class="card" id="card--${cardNumber++}"> <div>${text}</div><div><button id="delete--${cardNumber - 1}">Delete This Card</button></div></article>`
+        return createCard
+    },
+    deleteButton: document.querySelector(`#delete--${cardNumber}`),
+    card: document.querySelector(`#card--${cardNumber}`),
+    cardRemover: function () {
+       container.removeChild(this.card)
+
+    }
+}
+console.log(cardObject.deleteButton)
+
+
+console.log(cardNumber)
+document.querySelector("#create").addEventListener("click", cardObject.cardCreator)
+
+
+let cardName = document.querySelector(".card")
+
+document.querySelector("#delete--6").addEventListener("click", function test() {
+    container.removeChild(cardName)
+    console.log("test")
+})
+
+
+
+document.querySelector(`${cardObject.deleteButton}`).addEventListener("click", cardObject.cardRemover)
